f311e68bded38fe74f515d6004abdab4c592d402
Roopesh-N <roopeshnagineni123@outlook.com>
2024-06-14 02:23:55 -0400

    d

--- server.mjs
+++ server.mjs
@@ -11,11 +11,11 @@ app.use(express.json());
 const options = {
   method: 'GET',
   headers: {
-    'x-apikey': 'ca86edbbcc936767fb08ff6b3f11144ec98a617783223f4bdef8c1b7cfa6276b', 
+    'x-apikey': process.env.REACT_APP_VIRUS_TOTAL_KEY, 
   },
 };
 
-app.get('/api/domain/:domain', async (req,  res) => {
+app.get('/api/domain/:domain', async (req, res) => {
   const domain = req.params.domain;
   try {
     const response = await fetch(`https://www.virustotal.com/api/v3/domains/${domain}`, options);
@@ -30,6 +30,6 @@ app.get('/api/domain/:domain', async (req,  res) => {
   }
 });
 
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
